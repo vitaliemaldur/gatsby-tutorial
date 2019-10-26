f9da41c1c940357fe200f9fc7a8f44a10401af28
Vitalie Maldur <maldur.vitalie@gmail.com>
2019-10-26 16:54:33 +0300

    Step 3: components, plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default () => (
-  <div style={{ color: `purple` }}>
-    <ul>
-      <li>
-        <Link to="/contact/">Contact</Link>
-      </li>
-      <li>
-        <Link to="/about-css-modules/">About CSS modules</Link>
-      </li>
-    </ul>
+  <Layout>
     <Header headerText="Hello Gatsby!" />
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
+  </Layout>
 )
