089aee08e0ec5164c3933d97e35ee8a118d2e85f
Vitalie Maldur <maldur.vitalie@gmail.com>
2019-10-26 16:31:38 +0300

    Step 2: styling, global css, css modules

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+
 export default () => (
   <div style={{ color: `teal` }}>
     <Link to="/">Home</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,14 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
+    <ul>
+      <li>
+        <Link to="/contact/">Contact</Link>
+      </li>
+      <li>
+        <Link to="/about-css-modules/">About CSS modules</Link>
+      </li>
+    </ul>
     <Header headerText="Hello Gatsby!" />
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
